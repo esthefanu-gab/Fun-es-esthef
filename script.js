ed8c8a4f6e8ff1af980209a787db1e7896c3fbac
esthefanu-gab <b.esthefany@escola.pr.gov.br>
2026-07-28 09:29:36 -0300

    28

---
+++ script.js
@@ -0,0 +1,22 @@
+const caixaprincipal = document.queryselector(".caixa-principal");
+const caixaperguntas = document.queryselector(".caixa-perguntas"); 
+const caixaalternativas = document.queryselector(".caixa-alternativas");
+const caixaresultado = document.queryselector(".caixa-resultado");
+const textoresultado = document.queryselector(".texto-resultado");
+
+const perguntas = [
+    {
+    enunciado: "pergunta 1",
+    alternativas: [
+        "alternativa 1", 
+        "alternativa 2"
+    ] 
+},
+{
+    enunciado: "pergunta 1",
+    alternativas: [
+        "alternativa 1", 
+        "alternativa 2"
+    ] 
+}
+];
